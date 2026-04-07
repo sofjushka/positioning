5acd4b6278b51c5744f9c7d206ade18ce1201d62
Alexei Zhirnov <alexeizhirnov06@gmail.com>
2026-04-07 20:53:07 +0500

    УРААААААААААААААААААААААА

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const openModal = document.getElementById('openModal');
 const closeModal = document.getElementById('closeModal');
 const modalOverlay = document.getElementById('modalOverlay');
 
-const progressFill = document.getElementById('progressFill');
-const progressText = document.querySelector('.progress-text');
+const progressFill = document.querySelector('.red');
+// const progressText = document.querySelector('.progress-text');
 
 openModal.addEventListener('click', function() {
     modalOverlay.classList.add('active');
@@ -38,19 +38,10 @@ function startProgress() {
         }
         
         progressFill.style.width = progress + '%';
-        
-        const textPosition = progressText.offsetLeft;
-        const fillWidth = progressFill.offsetWidth;
-        
-        if (textPosition < fillWidth) {
-            progressText.style.color = 'white';
-        } else {
-            progressText.style.color = '#333';
-        }
     }, interval);
 }
 
 function resetProgress() {
     progressFill.style.width = '0%';
-    progressText.style.color = '#333';
+    // progressText.style.color = '#333';
 }
